b00acbb40c5e97cea01ca67badc390a97875f05d
SUITE <SUITE@SUITEA301-SERV.cibertec.edu.pe>
2023-08-24 21:26:41 -0500

    inicio

---
+++ src/main/java/com/empresa/DawiSemana01InicioApplication.java
@@ -0,0 +1,13 @@
+package com.empresa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiSemana01InicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiSemana01InicioApplication.class, args);
+	}
+
+}
